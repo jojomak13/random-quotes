7197849748b33afa4cc3e74db0cf4cc1af3a6c3a
jojomak13 <jojomak350@gmail.com>
2019-08-21 12:48:17 +0200

    First commit

---
+++ precache-manifest.9ebd674a3946c01ca6af2f32f6a23121.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb206fb180f44eca61e8b2e11e6a37f6",
+    "url": "/random-quotes/index.html"
+  },
+  {
+    "revision": "a4d9a640cba5b9317e80",
+    "url": "/random-quotes/static/css/main.80fb82a8.chunk.css"
+  },
+  {
+    "revision": "75670fe5764881f8f62a",
+    "url": "/random-quotes/static/js/2.5c181463.chunk.js"
+  },
+  {
+    "revision": "a4d9a640cba5b9317e80",
+    "url": "/random-quotes/static/js/main.319c737a.chunk.js"
+  },
+  {
+    "revision": "8bf32367f7bf80605e38",
+    "url": "/random-quotes/static/js/runtime~main.9d4d0ba3.js"
+  }
+]);
